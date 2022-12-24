59598e9687ace1dd8728ffc56723fc2424c9106c
ashutoshsalunke <ashutoshsalunke1711@gmail.com>
2022-12-24 21:31:26 +0530

    first commit : code

---
+++ src/com/velocity/demo/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+
+		ArrayList list = new ArrayList();
+		list.add("Ashutosh");
+		list.add("Vaibhav");
+		list.add("Kapil");
+		System.out.println(list);
+
+	}
+
+}
